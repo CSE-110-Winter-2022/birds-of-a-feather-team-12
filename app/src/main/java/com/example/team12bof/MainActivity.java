3143e477a4ef7f2219dc244d470857678aa61041
Saman9775 <s1zarei@ucsd.edu>
2022-02-06 14:35:30 -0800

    Saman and Ava first commit

--- app/src/main/java/com/example/team12bof/MainActivity.java
+++ app/src/main/java/com/example/team12bof/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+//Newonehgfhgfhgfhgfh
+//rewugruywegruryewguygerwu
 public class MainActivity extends AppCompatActivity {
 
     @Override
