3bb08cdfbad694cb99c7bbf08364cbfd078c6916
alelobo543 <alobo@ucsd.edu>
2022-02-07 00:57:09 -0800

    Mock Tests

--- app/src/test/java/com/example/team12bof/ExampleUnitTest.java
+++ app/src/test/java/com/example/team12bof/ExampleUnitTest.java
@@ -11,6 +11,7 @@ import com.example.team12bof.db.Course;
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ *
  */
 public class ExampleUnitTest {
     @Test
