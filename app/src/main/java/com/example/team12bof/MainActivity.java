de6bfc76f6346c7710a07937460901e11dde46e8
AvaH74 <ahamedi@ucsd.edu>
2022-02-09 11:17:43 -0800

    Stop and start search

--- app/src/main/java/com/example/team12bof/MainActivity.java
+++ app/src/main/java/com/example/team12bof/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 //Newonehgfhgfhgfhgfh
 //rewugruywegruryewguygerwu
@@ -19,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
     public void onAddClassClicked(View view) {
         Intent huh = new Intent(this, AddClassActivity.class);
         startActivity(huh);
+    }
+
+    public void onSearchClicked(View view){
+        Intent intent = new Intent(MainActivity.this, DemoService.class);
+        startService(intent);
+        Button start = (Button)findViewById(R.id.start);
+        start.setVisibility(View.INVISIBLE);
+        Button stop = (Button) findViewById(R.id.stop);
+        stop.setVisibility(View.VISIBLE);
+    }
+
+    public void onStopClicked(View view){
+        Intent intent = new Intent(MainActivity.this, DemoService.class);
+        stopService(intent);
+        Button stop = (Button) findViewById(R.id.stop);
+        stop.setVisibility(View.INVISIBLE);
+        Button start = (Button)findViewById(R.id.start);
+        start.setVisibility(View.VISIBLE);
+
     }
 }
