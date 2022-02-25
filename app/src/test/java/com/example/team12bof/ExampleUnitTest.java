b003289914e6dde38505f85daca8a908c69710ec
Jeannelle Balilo <jeannellebalilo@Jeannelles-MacBook-Pro.local>
2022-02-25 12:37:18 -0800

    fixed unit tests

--- app/src/test/java/com/example/team12bof/ExampleUnitTest.java
+++ app/src/test/java/com/example/team12bof/ExampleUnitTest.java
@@ -26,14 +26,14 @@ public class ExampleUnitTest {
     @Test
     public void testCourseNumber() {
         DummyStudent dummyStudent = new DummyStudent(0,"test");
-        Course course = new Course(0,"110","CSE","2022","Winter");
+        Course course = new Course(0,"110","CSE","2022","Winter", "Tiny (less than 40)");
         dummyStudent.addCourse(course);
         assertTrue("110".equals(dummyStudent.getCourses().get(0).getCourseNumber()));
     }
     @Test
     public void testYears() {
         DummyStudent dummyStudent = new DummyStudent(0,"test");
-        Course course = new Course(0,"110","CSE","2022","Winter");
+        Course course = new Course(0,"110","CSE","2022","Winter","Tiny (less than 40)");
         dummyStudent.addCourse(course);
         assertEquals("2022",dummyStudent.getCourses().get(0).getYear());
     }
